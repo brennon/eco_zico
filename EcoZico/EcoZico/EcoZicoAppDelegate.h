339853dbdabca021718929e75760c6e28d808a1f
Brennon Bortz <brennon@brennonbortz.com>
2011-04-21 17:45:32 +0100

    added EcoZico project directory

---
+++ EcoZico/EcoZico/EcoZicoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  EcoZicoAppDelegate.h
+//  EcoZico
+//
+//  Created by Brennon Bortz on 21/04/2011.
+//  Copyright 2011 Queen's University Belfast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EcoZicoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ EcoZico/EcoZico/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EcoZico
+//
+//  Created by Brennon Bortz on 21/04/2011.
+//  Copyright 2011 Queen's University Belfast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
