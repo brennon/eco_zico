f5933edfb1a07e67049463770d37ca07029b635f
Brennon Bortz <brennon@brennonbortz.com>
2011-07-02 12:43:18 +0100

    Backing out cocos2d files to debug compile issues

---
+++ EcoZico/EcoZico/GameConfig.h
@@ -0,0 +1,28 @@
+//
+//  GameConfig.h
+//  test
+//
+//  Created by Donal O'Brien on 01/05/2011.
+//  Copyright Queens University Belfast 2011. All rights reserved.
+//
+
+#ifndef __GAME_CONFIG_H
+#define __GAME_CONFIG_H
+
+//
+// Supported Autorotations:
+//		None,
+//		UIViewController,
+//		CCDirector
+//
+#define kGameAutorotationNone 0
+#define kGameAutorotationCCDirector 1
+#define kGameAutorotationUIViewController 2
+
+//
+// Define here the type of autorotation that you want for your game
+//
+#define GAME_AUTOROTATION kGameAutorotationUIViewController
+
+
+#endif // __GAME_CONFIG_H
