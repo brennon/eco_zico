dc9e63a542369857f56b171e7b636c42f64e6cd6
Brennon Bortz <brennon@brennonbortz.com>
2011-07-07 08:09:30 +0100

    csv parser actually working

--- csvlist/csvlist.rb
+++ csvlist/csvlist.rb
@@ -6,9 +6,14 @@ csv_file_path = File.expand_path("../seekpoints.csv", __FILE__)
 
 array_for_plist = []
 
+last_page = -1
+
 CSV.foreach(csv_file_path) do |row|
-  array_for_plist[row[0].to_i] ||= []
-  array_for_plist[row[0].to_i] << {"text"=>row[2],"time"=>row[3].to_f}
+  if last_page != row[0].to_i
+    last_page = row[0].to_i
+    array_for_plist << { "words" => [], "image" => "empty" }
+  end
+  array_for_plist[row[0].to_i]["words"] << {"text"=>row[2],"time"=>row[3].to_f}
 end
 
 plist_file = "#{__FILE__[0..(__FILE__.index('.')-1)]}.plist"
