5b6522b8a387881730c8eeb84a60d600aeb5e865
Brennon Bortz <brennon@brennonbortz.com>
2011-07-23 12:41:33 +0100

    Created front view controller files

---
+++ EcoZico/EcoZico/EZFrontViewController.h
@@ -0,0 +1,16 @@
+//
+//  EZFrontViewController.h
+//  EcoZico
+//
+//  Created by Brennon Bortz on 23/07/2011.
+//  Copyright 2011 Queen's University Belfast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface EZFrontViewController : UIViewController {
+    
+}
+
+@end
