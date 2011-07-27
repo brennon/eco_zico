d4438c84620cc559c0d9382fce7de33165badb18
Brennon Bortz <brennon@brennonbortz.com>
2011-07-27 14:40:50 +0100

    Added EZHelpViewController and xib

---
+++ EcoZico/EcoZico/EZHelpViewController.h
@@ -0,0 +1,16 @@
+//
+//  EZHelpViewController.h
+//  EcoZico
+//
+//  Created by Brennon Bortz on 27/07/2011.
+//  Copyright 2011 Queen's University Belfast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface EZHelpViewController : UIViewController {
+    
+}
+
+@end
