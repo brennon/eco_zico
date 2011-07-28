f8c54636afbbfee7dade4f4527fa0fa0614c76e3
Brennon Bortz <brennon@brennonbortz.com>
2011-07-28 18:02:15 +0100

    Help animations in place

--- EcoZico/EcoZico/EZHelpViewController.h
+++ EcoZico/EcoZico/EZHelpViewController.h
@@ -13,11 +13,19 @@
     IBOutlet UIImageView *handView;
     IBOutlet UIView *screenView;
     IBOutlet UIImageView *textView;
+    IBOutlet UIButton *nextButton;
+    
+    NSNumber *currentInstructions;
+    NSArray *textImageFilenames;
+    CGRect handRects[4];
 }
 
 @property (nonatomic, retain) IBOutlet UIImageView *handView;
 @property (nonatomic, retain) IBOutlet UIView *screenView;
 @property (nonatomic, retain) IBOutlet UIImageView *textView;
+@property (nonatomic, retain) NSNumber *currentInstructions;
+@property (nonatomic, retain) NSArray *textImageFilenames;
+@property (nonatomic, retain) IBOutlet UIButton *nextButton;
 
 - (IBAction)nextButtonPushed:(id)sender;
 - (IBAction)exitButtonPushed:(id)sender;
--- EcoZico/EcoZico/EZHelpViewController.m
+++ EcoZico/EcoZico/EZHelpViewController.m
@@ -11,25 +11,36 @@
 
 
 @implementation EZHelpViewController
+@synthesize nextButton;
 
-@synthesize screenView, textView, handView;
+@synthesize screenView, textView, handView, currentInstructions, textImageFilenames;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        self.currentInstructions = [NSNumber numberWithInt:0];
+        self.textImageFilenames = [NSArray arrayWithObjects:
+                                   @"instructions_01.png", 
+                                   @"instructions_02.png", 
+                                   @"instructions_03.png",
+                                   @"instructions_04.png", 
+                                   nil];
+        
+        handRects[0] = CGRectMake(236, 570, 293, 475);
+        handRects[1] = CGRectMake(-136, 320, 293, 475);
+        handRects[2] = CGRectMake(236, 570, 293, 475);
     }
     return self;
 }
 
 - (void)dealloc
 {
-    [handView release];
-    [handView release];
+    [currentInstructions release];
     [handView release];
     [screenView release];
     [textView release];
+    [nextButton release];
     [super dealloc];
 }
 
@@ -55,6 +66,7 @@
     [self setHandView:nil];
     [self setScreenView:nil];
     [self setTextView:nil];
+    [self setNextButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -66,8 +78,47 @@
 	return YES;
 }
 
-- (IBAction)nextButtonPushed:(id)sender
+- (void)animateScreenshot {}
+
+- (void)animateText
+{  
+    [UIView animateWithDuration:0.75 
+                     animations:^ { 
+                         self.textView.alpha = 0.0; 
+                     } 
+                     completion:^(BOOL finished) {
+                         NSString *nextFilename = [self.textImageFilenames objectAtIndex:[currentInstructions intValue]];    
+                         UIImage *nextTextImage = [UIImage imageNamed:nextFilename];    
+                         self.textView.image = nextTextImage;
+                         [UIView animateWithDuration:0.75 
+                                          animations:^ {
+                                              self.textView.alpha = 1.0;  
+                                          }];
+                     }];
+}
+
+- (void)animateHand
 {
+    [UIView animateWithDuration:1.0 
+                     animations:^ {
+                         self.handView.frame = handRects[[currentInstructions intValue] - 1];
+                     }];
+}
+
+- (void)animateHelpScreen
+{
+    [self animateScreenshot];
+    [self animateText];
+    [self animateHand];
+}
+
+- (IBAction)nextButtonPushed:(id)sender
+{    
+    self.currentInstructions = [NSNumber numberWithInt:[currentInstructions intValue] + 1];
+    if ([self.currentInstructions intValue] == 3) {
+        [self.nextButton removeFromSuperview];
+    }
+    [self animateHelpScreen];
 }
 
 - (IBAction)exitButtonPushed:(id)sender
